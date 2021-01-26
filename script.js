01b2c7a52554430fb8c28c4c95dcaf97328249f9
fortanza <undertaker040880@gmail.com>
2021-01-26 08:33:23 +0100

    dernier commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = "";
+
+function ShowValue(btn){
+    input += btn.value;
+    document.getElementById("output").innerHTML = input;
+}
+
+function quickMath(){
+
+    document.getElementById("output").innerHTML = eval(input);
+}
+
+function Reset(btn){
+    document.getElementById("output").innerHTML = "0";
+    input = "";
+}
+
+function rem1(btn){
+    input = input.replace(input.slice(-1), "");
+    document.getElementById("output").innerHTML = input;
+}
